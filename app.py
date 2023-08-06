a993ee2118d117dc748c8b82e7c9feb0deb4ed43
Arthur Burgin Jr <artyymcflyy@users.noreply.github.com>
2023-08-06 19:33:34 -0400

    setup app server to load config values dynamically

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
 
+from config import Environemnt, load_config
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!!</p>"
+@app.get("/health-check")
+def health_check():
+    return "Hello World!!"
+
+if __name__ == '__main__':
+    config = load_config()
+    debug_mode = False if config.ENVIRONMENT == Environemnt.PRODUCTION else True
+    print(f"Loading {config.ENVIRONMENT} config values")
+    print(f"Debug Mode: {debug_mode}")
+    
+    app.config["ENVIRONMENT"] = config.ENVIRONMENT
+    app.run(host=config.HOST, port=config.PORT, debug=debug_mode)
